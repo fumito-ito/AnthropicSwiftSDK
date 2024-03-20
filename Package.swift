7f53075f96646f56808e239f5da5f0a0781d397e
Fumito Ito <weathercook@gmail.com>
2024-03-20 22:12:48 +0900

    use xcode 14 for Swift 5.9

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "AnthropicSwiftSDK",
-    platforms: [.iOS(.v16)],
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
