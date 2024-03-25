17db46f931274935604a3d47bca5186f250f8ca3
Fumito Ito <weathercook@gmail.com>
2024-03-26 00:24:32 +0900

    fix nits

--- Sources/AnthropicSwiftSDK-Bedrock/Extensions/MessagesResponse+Extension.swift
+++ Sources/AnthropicSwiftSDK-Bedrock/Extensions/MessagesResponse+Extension.swift
@@ -12,7 +12,7 @@ import AWSBedrockRuntime
 extension MessagesResponse {
     init (from invokeModelOutput: InvokeModelOutput) throws {
         guard let data = invokeModelOutput.body else {
-            throw AnthropicBedrockClientError.cannotGetAnyDataFromBedrockResponse(invokeModelOutput)
+            throw AnthropicBedrockClientError.cannotGetAnyDataFromBedrockMessageResponse(invokeModelOutput)
         }
 
         self = try anthropicJSONDecoder.decode(MessagesResponse.self, from: data)
