a1f448407240f36ed7e21f786957268d71675ac8
Fumito Ito <weathercook@gmail.com>
2024-09-06 01:08:16 +0900

    make SystemPrompt Encodable to take it on request

--- Sources/AnthropicSwiftSDK/Entity/SystemPrompt.swift
+++ Sources/AnthropicSwiftSDK/Entity/SystemPrompt.swift
@@ -7,11 +7,28 @@
 
 import Foundation
 
-public enum CacheControl {
+public enum CacheControl: String, Encodable {
     /// corresponds to this 5-minute lifetime.
     case ephemeral
 }
 
 public enum SystemPrompt {
     case text(String, CacheControl?)
+}
+
+extension SystemPrompt: Encodable {
+    enum CodingKeys: CodingKey {
+        case text
+        case cacheControl
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        guard case let .text(text, cacheControl) = self else {
+            fatalError()
+        }
+
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(text, forKey: .text)
+        try container.encode(cacheControl, forKey: .cacheControl)
+    }
 }
