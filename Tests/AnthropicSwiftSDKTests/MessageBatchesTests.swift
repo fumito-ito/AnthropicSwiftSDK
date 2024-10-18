914bee6e5ca2a8e369bcf0c91dd8d7458ad75854
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-10-18 15:24:17 +0900

    add empty tests

---
+++ Tests/AnthropicSwiftSDKTests/MessageBatchesTests.swift
@@ -0,0 +1,13 @@
+//
+//  MessageBatchesTests.swift
+//  AnthropicSwiftSDK
+//
+//  Created by 伊藤史 on 2024/10/18.
+//
+
+import XCTest
+@testable import AnthropicSwiftSDK
+
+final class MessageBatchesTests: XCTestCase {
+    // TODO: write tests
+}
---
+++ Tests/AnthropicSwiftSDKTests/Network/BatchResultResponseTests.swift
@@ -0,0 +1,13 @@
+//
+//  BatchResultResponseTests.swift
+//  AnthropicSwiftSDK
+//
+//  Created by 伊藤史 on 2024/10/18.
+//
+
+import XCTest
+@testable import AnthropicSwiftSDK
+
+final class BatchResultResponseTests: XCTestCase {
+    // TODO: write tests
+}
