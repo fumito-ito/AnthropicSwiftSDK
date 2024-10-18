f43652532d365e2f1d23987d2e221fa8e325e940
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-10-18 14:48:35 +0900

    add interface for message batches api

--- Sources/AnthropicSwiftSDK/Anthropic.swift
+++ Sources/AnthropicSwiftSDK/Anthropic.swift
@@ -11,10 +11,14 @@ import Foundation
 public final class Anthropic {
     /// Messages API Interface
     public let messages: Messages
+    
+    /// MessageBatches API Interface
+    public let messageBatches: MessageBatches
 
     /// Construction of SDK
     /// - Parameter apiKey: API key to access Anthropic API.
     public init(apiKey: String) {
         self.messages = Messages(apiKey: apiKey, session: .shared)
+        self.messageBatches = MessageBatches(apiKey: apiKey, session: .shared)
     }
 }
