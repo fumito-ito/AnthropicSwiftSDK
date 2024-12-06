a1591ac18acdf8a8b1044accc96a40b695159dfc
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-12-06 13:01:38 +0900

    move test files

--- Tests/AnthropicSwiftSDKTests/Network/Response/ObjectListResponseTests.swift
+++ Tests/AnthropicSwiftSDKTests/Network/Response/ObjectListResponseTests.swift
@@ -1,5 +1,5 @@
 //
-//  BatchListResponseTests.swift
+//  ObjectListResponseTests.swift
 //  AnthropicSwiftSDK
 //
 //  Created by 伊藤史 on 2024/10/18.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import AnthropicSwiftSDK
 
-final class BatchListResponseTests: XCTestCase {
+final class ObjectListResponseTests: XCTestCase {
     func testDecodeBatchListResponse() throws {
         let json = """
         {
